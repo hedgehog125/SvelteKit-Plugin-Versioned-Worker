243c711677972e7cab4a921ce0d5b482fb8f67fa
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-12-09 23:39:58 +0000

    Modified the wrong file, this should work though?

---
+++ app/immutable/chunks/1-0cad282c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-b3ba051a.js";export{t as component};
--- app/immutable/chunks/1-ddcda609.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-ff5cb705.js";export{t as component};
