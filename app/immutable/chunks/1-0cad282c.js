88576cd3fc6c26558d225f533e4b98b20d6afb44
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-12-10 19:43:01 +0000

    Added a hidden page that uses the hook system

--- app/immutable/chunks/1-0cad282c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-b3ba051a.js";export{t as component};
---
+++ app/immutable/chunks/1-aca06cac.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-a111bac7.js";export{t as component};
